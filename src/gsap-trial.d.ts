b7379188c66c191400c48c4f5430d93f87d4b7d7
harekamk <harekamk@gmail.com>
2026-08-18 21:28:22 +0530

    Add TypeScript declarations for gsap-trial modules

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,10 @@
+declare module "gsap-trial/SplitText" {
+  const content: any;
+  export const SplitText: any;
+  export default content;
+}
+declare module "gsap-trial/ScrollSmoother" {
+  const content: any;
+  export const ScrollSmoother: any;
+  export default content;
+}
